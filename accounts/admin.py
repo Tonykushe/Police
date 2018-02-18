ac08f4a65d4b2f7c0fd5e3f450e24f0a47db2bb8
Tony Mugendi <mugendi.tony26@gmail.com>
2018-02-18 18:32:39 +0300

    Customized the admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from . models import OfficerProfile
+
 # Register your models here.
-admin.site.register(OfficerProfile)
+admin.site.site_header = 'Traffic Offence Doc System'
+
+class OfficerProfileAdmin(admin.ModelAdmin):
+	list_display = ('user', 'description', 'phone', 'location')
+	search_fields = ['user', 'location']
+
+admin.site.register(OfficerProfile, OfficerProfileAdmin)
